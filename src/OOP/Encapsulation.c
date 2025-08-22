cb0ac4cbe6f84178adfd15752c875dcc1f69f5f1
Nguyễn Quý Ngọc <76089776+azzo-dude@users.noreply.github.com>
2025-08-23 00:39:18 +0700

    Create Encapsulation.c

---
+++ src/OOP/Encapsulation.c
@@ -0,0 +1,14 @@
+#include <"stdio.h">
+
+#define class struct
+
+typedef class cat {
+    char *name;
+    int  *age;  
+} Cat;
+
+int main() {
+  Cat cat = {.name = "NYAN", .age= 7};
+  printf(""This my cat : %s, his is now %d year old.", cat.name, cat.age);
+  return 0;
+}
